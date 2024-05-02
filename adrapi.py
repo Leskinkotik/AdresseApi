745ca483400cc80a10dfc850413b439478a522a0
Lesia <leskinkotik@gmail.com>
2024-05-02 14:54:03 +0200

    🚀 addin script for looking the status

--- adrapi.py
+++ adrapi.py
@@ -1,2 +1,13 @@
-import json
-#wertyuiopp;lkjhgf
+import requests
+
+url = "https://api-adresse.data.gouv.fr/reverse/?lon=2.234489&lat=48.814737"
+
+response = requests.get(url)
+
+# I need status 200
+if response.status_code == 200:
+    info = response.json()
+    print(info['type'])
+else:
+    print("error:", response.status_code)
+
